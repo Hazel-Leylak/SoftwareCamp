2812d55e7848e3de4ebaa01e8d5850d6073a584c
Hazel Leylak <hazel.leylak@gmail.com>
2021-01-21 20:48:12 +0300

    Add project files.

---
+++ Example_3_1/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example_3_1
+{
+    class MyDictionary<K, V>
+    {
+
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] _tempKeyArray = keys;
+            V[] _tempValArray = values;
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                keys[i] = _tempKeyArray[i];
+                values[i] = _tempValArray[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void PrintValues()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ", " + values[i]);
+            }
+
+        }
+    }
+}
---
+++ Example_3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Sydney, Melbourne");
+            myDictionary.Add(3, "Kanberra");
+            myDictionary.PrintValues();
+            Console.ReadKey();
+        }
+    }
+}
